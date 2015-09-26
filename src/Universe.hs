13c126a2d3dd339ad57c9b23b36bacb8fe110e2e
Martin Kolinek <martin.kolinek@gmail.com>
2015-09-26 22:51:21 +0200

    Initial operations

--- src/Universe.hs
+++ src/Universe.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE RankNTypes      #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
 
-module Universe where
+module Universe (initialUniverse, startWorking, finishTurn, Universe, WorkerId, WorkplaceId) where
 
 import           Control.Lens
 import           Control.Lens.TH
 import           Control.Monad
-import           Data.Map.Strict
+import           Data.Map.Strict as M hiding ((\\))
 import           Data.Maybe
+import           Data.List
 import           Prelude         hiding (lookup)
+import           Control.Monad.Except
 
 data Universe = Universe {
   _availableWorkplaces :: Map WorkplaceId WorkplaceAction,
@@ -22,6 +25,8 @@ data WorkerState = WorkerState {
   _currentWorkplace :: Maybe WorkplaceId
 }
 
+initialWorkerState = WorkerState Nothing
+
 newtype WorkplaceId = WorkplaceId Int deriving (Eq, Ord)
 
 data WorkplaceAction = IncreaseScore
@@ -32,15 +37,38 @@ makeLenses ''WorkerState
 workerState :: WorkerId -> Lens' Universe (Maybe WorkerState)
 workerState workerId = workers . at workerId
 
-startWorking :: WorkerId -> WorkplaceId -> Universe -> Universe
-startWorking workerId workplaceId universe =
-  if workerExists && workerIdle && workplaceEmpty
-    then over currentWorkerState (liftM $ set currentWorkplace $ Just workplaceId) universe
-    else universe
+workerWorking :: WorkerState -> Bool
+workerWorking = isJust . view currentWorkplace
+
+freeWorkplaces :: Universe -> [WorkplaceId]
+freeWorkplaces universe = universeAvailableWorkplaces \\ universeOccupiedWorkplaces
+  where universeOccupiedWorkplaces = catMaybes $ view currentWorkplace <$> workerStates
+        universeAvailableWorkplaces = keys $ view availableWorkplaces universe
+        workerStates = elems (view workers universe)
+
+check :: MonadError e m => Bool -> e -> m ()
+check True _ = return ()
+check False e = throwError e
+
+startWorking :: MonadError String m => WorkerId -> WorkplaceId -> Universe -> m Universe
+startWorking workerId workplaceId universe = do
+  check workerExists "Worker does not exist"
+  check workerIdle "Worker already working"
+  check workplaceEmpty "Workplace occupied"
+  return $ over currentWorkerState setWorkplace universe
   where currentWorkerState :: Lens' Universe (Maybe WorkerState)
         currentWorkerState = workerState workerId
         workerExists = isJust $ view currentWorkerState universe
         workerIdle = isNothing $ do
           workState <- view currentWorkerState universe
           view currentWorkplace workState
-        workplaceEmpty =
+        workplaceEmpty = workplaceId `elem` freeWorkplaces universe
+        setWorkplace = liftM $ set currentWorkplace $ Just workplaceId
+
+initialUniverse = Universe (fromList [(WorkplaceId 1, IncreaseScore)]) (fromList [(WorkerId 1, initialWorkerState)]) 0
+
+finishTurn :: MonadError String m => Universe -> m Universe
+finishTurn universe = do
+  check allWorkersBusy "Not all workers are busy"
+  return $ over workers (M.map (const initialWorkerState)) universe
+  where allWorkersBusy = all workerWorking (elems (view workers universe))
