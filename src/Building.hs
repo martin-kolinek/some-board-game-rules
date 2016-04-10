56c904d783d2d786c87e7cab59af45057f387670
Martin Kolinek <martin.kolinek@gmail.com>
2016-04-10 16:36:27 +0200

    Building occupants

--- src/Building.hs
+++ src/Building.hs
@@ -8,6 +8,10 @@ import Data.Maybe
 import Control.Monad.Except
 import Data.List
 import Control.Exception
+import Worker
+import Control.Monad.Writer
+import qualified Data.Map as M
+import Control.Lens
 
 type Position = (Int, Int)
 
@@ -15,14 +19,14 @@ data Building =
   Forest Position |
   Grass Position |
   Rock Position |
-  InitialRoom Position
+  InitialRoom Position deriving (Show, Eq)
 
 buildingPositions (Forest pos) = [pos]
 buildingPositions (Grass pos) = [pos]
 buildingPositions (Rock pos) = [pos]
 buildingPositions (InitialRoom pos) = [pos]
 
-newtype BuildingSpace = BuildingSpace [Building]
+newtype BuildingSpace = BuildingSpace [Building] deriving (Show, Eq)
 
 initialBuildingSpace = [Forest (0, 0), Forest (1, 0), Forest (0, 1), Forest (1, 1), Rock (2, 0), Rock (3, 0), Rock (3, 1), InitialRoom (2, 1)]
 
@@ -48,3 +52,23 @@ cutForest position buildingSpace = do
   building <- checkMaybe (getBuilding buildingSpace position) "Invalid position"
   check (isForest building) "Cutting forest not in a forest"
   return $ build (Grass position) buildingSpace
+
+data BuildingOccupant = WorkerOccupant WorkerId deriving Eq
+
+type BuildingOccupants = M.Map Position [BuildingOccupant]
+
+type OccupantError = (String, Position)
+
+areBuildingOccupantsValid :: MonadWriter [OccupantError] m => Building -> [BuildingOccupant] -> m ()
+areBuildingOccupantsValid (InitialRoom pos) occupants = checkWriter (length occupants <= 2) ("Too many people", pos)
+areBuildingOccupantsValid building occupants = checkWriter (null occupants) ("There can't be anyone here", head $ buildingPositions building)
+
+areOccupantsValid :: [BuildingOccupant] -> BuildingSpace -> BuildingOccupants -> [OccupantError]
+areOccupantsValid allOccupants (BuildingSpace buildings) occupants = snd $ runWriter $ do
+  forM_ buildings $ \building -> do
+    let positions = buildingPositions building
+        buildingOccupants = concat $ catMaybes $ (`M.lookup` occupants) <$> positions
+    areBuildingOccupantsValid building buildingOccupants
+  let positionedOccupants = concat $ M.elems occupants
+  checkWriter (positionedOccupants `intersect` allOccupants == allOccupants) ("Not everyone has a place", (0, 0))
+  return ()
--- src/Util.hs
+++ src/Util.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Util where
 
 import Control.Monad.Except
+import Control.Monad.Writer
 
 check :: MonadError e m => Bool -> e -> m ()
 check True _ = return ()
@@ -9,3 +12,7 @@ check False e = throwError e
 checkMaybe :: MonadError e m => Maybe a -> e -> m a
 checkMaybe Nothing e = throwError e
 checkMaybe (Just x) _ = return x
+
+checkWriter :: MonadWriter [a] m => Bool -> a -> m ()
+checkWriter True _ = return ()
+checkWriter False a = tell [a]
