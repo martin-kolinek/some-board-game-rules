7427cdc6cd9fc04674c618bc539e9430611db16a
Martin Kolinek <martin.kolinek@gmail.com>
2016-10-21 22:07:13 +0200

    Monadic approach to testing with quick check

--- test/TestFramework.hs
+++ test/TestFramework.hs
@@ -5,10 +5,14 @@ module TestFramework where
 import Rules
 
 import Test.Tasty
-import Test.Tasty.HUnit as H
+import Test.Tasty.HUnit as H hiding (assert)
 import Control.Monad.State
 import Data.Map.Strict
 import Control.Monad.Except
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
+import Generators
+import Text.Show.Pretty
 
 type FlowTest t = ExceptT String (StateT t IO)
 
@@ -45,3 +49,31 @@ getWorkplace number = (!! number) . keys . getWorkplaces <$> get
 
 apply :: (forall m. MonadError String m => (a -> m a)) -> FlowTest a ()
 apply action = put =<< action =<< get
+
+type UniversePropertyMonad = PropertyM (ExceptT String (State Universe))
+
+data ResultExpectation = GameSuccess | GameFailure deriving (Show, Eq)
+
+universeProperty :: ResultExpectation -> UniversePropertyMonad a -> ArbitraryUniverse -> Property
+universeProperty expectation action (ArbitraryUniverse universe) = monadic extractProperty (action >> (assert $ expectation == GameSuccess))
+  where extractProperty act = checkEither $ runState (runExceptT act) universe
+        checkEither (Right prop, resultUniverse) =
+          counterexample ("Resulting universe: " ++ ppShow resultUniverse) $
+          prop .||.
+          (counterexample "Didn't expect success" $ expectation == GameSuccess)
+        checkEither (Left msg, resultUniverse) =
+          counterexample ("Resulting universe: " ++ ppShow resultUniverse) $
+          counterexample ("Unexpected error: " ++ msg) $
+          expectation == GameFailure
+
+getUniverse :: UniversePropertyMonad Universe
+getUniverse = run get
+
+getsUniverse :: (Universe -> a) -> UniversePropertyMonad a
+getsUniverse x = run $ gets x
+
+applyToUniverse :: (forall m. MonadError String m => Universe -> m Universe) -> UniversePropertyMonad ()
+applyToUniverse action = do
+  u <- getUniverse
+  nextU <- run $ action u
+  run $ put nextU
